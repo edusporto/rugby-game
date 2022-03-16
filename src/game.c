6801e3e161ec36277be40f8ce54fac9b9d8dde28
Renato Cordeiro Ferreira <renato.cferreira@hotmail.com>
2022-03-16 00:56:42 -0300

    Add option to run game from a loaded map

--- src/game.c
+++ src/game.c
@@ -1,16 +1,19 @@
 // Standard headers
+#include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 // Internal headers
 #include "field.h"
+#include "map.h"
 #include "spy.h"
 
 // Main header
 #include "game.h"
 
 // Macros
+#define MAX_SINGLE_OCCURRENCE 1UL
 #define UNUSED(x) (void)(x) // Auxiliary to avoid error of unused parameter
 
 /*----------------------------------------------------------------------------*/
@@ -37,6 +40,16 @@ struct game {
 /*                          PRIVATE FUNCTIONS HEADERS                         */
 /*----------------------------------------------------------------------------*/
 
+Game allocate_game(
+  dimension_t field_dimension,
+  size_t max_number_spies,
+  PlayerStrategy execute_attacker_strategy,
+  PlayerStrategy execute_defender_strategy);
+
+bool has_map_exceeded_max_occurrences_of_symbol(
+    Map map, char symbol, size_t max_occurrences);
+void set_item_in_field_from_map(Field field, Item item, Map map);
+
 void set_attacker_in_field(Field field, Item attacker);
 void set_defender_in_field(Field field, Item defender);
 void set_obstacles_in_field(Field field, Item obstacle);
@@ -60,25 +73,55 @@ Game new_game(
     size_t max_number_spies,
     PlayerStrategy execute_attacker_strategy,
     PlayerStrategy execute_defender_strategy) {
-  Game game = malloc(sizeof(*game));
-
-  game->field = new_field(field_dimension);
-
-  game->max_number_spies = max_number_spies;
-
-  game->execute_attacker_strategy = execute_attacker_strategy;
-  game->execute_defender_strategy = execute_defender_strategy;
-
-  game->attacker = new_item('A', true);
-  game->defender = new_item('D', true);
-  game->obstacle = new_item('X', false);
-
-  game->attacker_spy = new_spy(game->attacker);
-  game->defender_spy = new_spy(game->defender);
+  Game game = allocate_game(
+      field_dimension,
+      max_number_spies,
+      execute_attacker_strategy,
+      execute_defender_strategy);
 
   set_attacker_in_field(game->field, game->attacker);
   set_defender_in_field(game->field, game->defender);
   set_obstacles_in_field(game->field, game->obstacle);
+
+  return game;
+}
+
+/*----------------------------------------------------------------------------*/
+
+Game new_game_from_map(
+    Map map,
+    size_t max_number_spies,
+    PlayerStrategy execute_attacker_strategy,
+    PlayerStrategy execute_defender_strategy) {
+  if (map == NULL) return NULL;
+
+  dimension_t field_dimension = get_map_dimension(map);
+
+  Game game = allocate_game(
+      field_dimension,
+      max_number_spies,
+      execute_attacker_strategy,
+      execute_defender_strategy);
+
+  if (has_map_exceeded_max_occurrences_of_symbol(
+        map, get_item_symbol(game->attacker), MAX_SINGLE_OCCURRENCE)) {
+    fprintf(stderr, "ERROR: Map exceeded max occurrences of symbol %c\n",
+        get_item_symbol(game->attacker));
+    delete_game(game);
+    return NULL;
+  }
+
+  if (has_map_exceeded_max_occurrences_of_symbol(
+        map, get_item_symbol(game->defender), MAX_SINGLE_OCCURRENCE)) {
+    fprintf(stderr, "ERROR: Map exceeded max occurrences of symbol %c\n",
+        get_item_symbol(game->defender));
+    delete_game(game);
+    return NULL;
+  }
+
+  set_item_in_field_from_map(game->field, game->attacker, map);
+  set_item_in_field_from_map(game->field, game->defender, map);
+  set_item_in_field_from_map(game->field, game->obstacle, map);
 
   return game;
 }
@@ -178,6 +221,82 @@ void play_game(Game game, size_t max_turns) {
 
 /*----------------------------------------------------------------------------*/
 /*                             PRIVATE FUNCTIONS                              */
+/*----------------------------------------------------------------------------*/
+
+Game allocate_game(
+    dimension_t field_dimension,
+    size_t max_number_spies,
+    PlayerStrategy execute_attacker_strategy,
+    PlayerStrategy execute_defender_strategy) {
+  Game game = malloc(sizeof(*game));
+
+  game->field = new_field(field_dimension);
+
+  game->max_number_spies = max_number_spies;
+
+  game->execute_attacker_strategy = execute_attacker_strategy;
+  game->execute_defender_strategy = execute_defender_strategy;
+
+  game->attacker = new_item('A', true);
+  game->defender = new_item('D', true);
+  game->obstacle = new_item('X', false);
+
+  game->attacker_spy = new_spy(game->attacker);
+  game->defender_spy = new_spy(game->defender);
+
+  return game;
+}
+
+/*----------------------------------------------------------------------------*/
+
+bool has_map_exceeded_max_occurrences_of_symbol(Map map,
+                                                char symbol,
+                                                size_t max_occurrences) {
+  if (max_occurrences == 0) return false;
+
+  dimension_t map_dimension = get_map_dimension(map);
+
+  size_t item_symbol_occurrences = 0;
+  for (size_t i = 0; i < map_dimension.height; i++) {
+    for (size_t j = 0; j < map_dimension.width; j++) {
+      position_t current_position = { i, j };
+      char current_map_symbol = get_map_symbol(map, current_position);
+
+      if (current_map_symbol == symbol) {
+        item_symbol_occurrences++;
+      }
+    }
+  }
+
+  return item_symbol_occurrences > max_occurrences ? true : false;
+}
+
+/*----------------------------------------------------------------------------*/
+
+void set_item_in_field_from_map(Field field,
+                                Item item,
+                                Map map) {
+  char item_symbol = get_item_symbol(item);
+
+  dimension_t map_dimension = get_map_dimension(map);
+  dimension_t field_dimension = get_field_dimension(field);
+
+  // By design, field should have the same dimension as the map
+  assert(field_dimension.height == map_dimension.height);
+  assert(field_dimension.width == map_dimension.width);
+
+  for (size_t i = 0; i < map_dimension.height; i++) {
+    for (size_t j = 0; j < map_dimension.width; j++) {
+      position_t current_position = { i, j };
+      char current_map_symbol = get_map_symbol(map, current_position);
+
+      if (current_map_symbol == item_symbol) {
+        add_item_to_field(field, item, current_position);
+      }
+    }
+  }
+}
+
 /*----------------------------------------------------------------------------*/
 
 void set_attacker_in_field(Field field, Item attacker) {
